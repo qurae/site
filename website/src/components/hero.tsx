b5c3e9c041722309a4061952ea56e01396794a33
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2025-08-29 17:07:05 +0100

    Tweaks

--- website/src/components/hero.tsx
+++ website/src/components/hero.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 
 import { cn } from "@/src/lib/utils";
 
-const font = Gabarito({ weight: "500", subsets: ["latin"] });
+const font = Gabarito({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 export const Hero = () => {
   const { resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
@@ -20,32 +20,32 @@ export const Hero = () => {
       <div className="mx-auto max-w-2xl py-32 sm:py-32 lg:py-12">
         <div className="mt-32 mb-8 flex justify-center">
           {/* Light mode logo */}
-          <img 
+          {/* <img 
             alt="Qurae Logo" 
             src="./logos/logo.svg" 
             className={cn(
               "h-16 w-auto transition-opacity duration-200",
               mounted && resolvedTheme === "dark" ? "opacity-0 absolute" : "opacity-100"
             )}
-          />
+          /> */}
           {/* Dark mode logo */}
-          <img 
+          {/* <img 
             alt="Qurae Logo" 
             src="./logos/logo-white.svg" 
             className={cn(
               "h-16 w-auto transition-opacity duration-200",
               mounted && resolvedTheme === "dark" ? "opacity-100" : "opacity-0 absolute"
             )}
-          />
+          /> */}
         </div>
         <div className="text-left max-w-4xl mx-auto">
           <p
             className={cn(
-              "text-black dark:text-white text-lg leading-relaxed mb-6",
+              "text-black dark:text-white text-4xl leading-relaxed mb-6 font-light",
               font.className
             )}
           >
-            We help organisations make their health data usable, safe, and connected.
+            Introducing <span className="font-semibold">Qurae</span>
           </p>
           <p
             className={cn(
@@ -53,16 +53,15 @@ export const Hero = () => {
               font.className
             )}
           >
-            Through trusted open-source tools and proven frameworks, we enable data strategy, federation, and insight across silos. Our team has a track record of unlocking the value of health data for research, machine learning, and real-world decision-making. 
-            Always with security, control, and governance at the core.
+            We’ve worked on programmes with the NHS, HDR UK, DARE UK, and NIHR.
           </p>
           <p
             className={cn(
-              "text-black dark:text-white text-lg",
+              "text-gray-600 dark:text-gray-300 text-base leading-relaxed mb-8",
               font.className
             )}
-          >
-            <a href="mailto:hello@qurae.co.uk" className="hover:text-gray-600 dark:hover:text-gray-300 transition-colors">hello@qurae.co.uk</a>
+          > The next step is open, help us define it:{' '}
+            <a href="mailto:hello@qurae.co.uk" className="hover:text-gray-600 dark:hover:text-gray-300 transition-colors text-black dark:text-white font-semibold underline underline-offset-8">hello@qurae.co.uk</a>
           </p>
           
         </div>
