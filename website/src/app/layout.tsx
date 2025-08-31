2a2cf8130bd0b6b28b344a5b05628c1b6fd0b200
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2025-08-31 22:07:21 +0100

    Hardcode image

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Qurae",
     description: "Something new is emerging from the University of Nottingham, led by the team that has delivered programmes with the NHS, Health Data Research UK, DARE UK, and the NIHR Nottingham Biomedical Research Centre.",
-    images: "/images/og-image.png",
+    images: "https://qurae.co.uk/images/og-image.png",
   },
   manifest: "/manifest.json",
 };
