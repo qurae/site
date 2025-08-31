f1a06c73be29a8de53e69027b47802a9fb3a3b4c
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2025-08-31 21:57:55 +0100

    Add metadata

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,10 +4,15 @@ import { ThemeProvider } from "@/src/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Qurae",
-  description: "...",
+  description: "Something new is emerging from the University of Nottingham, led by the team that has delivered programmes with the NHS, Health Data Research UK, DARE UK, and the NIHR Nottingham Biomedical Research Centre.",
   icons: {
-    icon: "/icons/favicon.ico",
-    apple: "/icons/apple-touch-icon.png",
+    icon: "./icons/favicon.ico",
+    apple: "./icons/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Qurae",
+    description: "Something new is emerging from the University of Nottingham, led by the team that has delivered programmes with the NHS, Health Data Research UK, DARE UK, and the NIHR Nottingham Biomedical Research Centre.",
+    images: "./images/og-image.png",
   },
   manifest: "/manifest.json",
 };
