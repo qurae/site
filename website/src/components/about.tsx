39feea07067e3dd5b3c8187b7723284ae1dfc8eb
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2025-12-02 20:45:37 +0000

    Fix colours

--- website/src/components/about.tsx
+++ website/src/components/about.tsx
@@ -13,23 +13,23 @@ export const About = () => {
     <section id="about" className={`py-16 px-6 lg:px-8 bg-slate-950`}>
       <div className="mx-auto max-w-4xl">
         <div className={cn("text-left", font.className)}>
-          <Heading className="text-5xl md:text-7xl mb-8">
+          <Heading className="text-5xl md:text-7xl mb-8 text-white">
             About Qurae
           </Heading>
           
-          <BodyText>
+          <BodyText className="text-white">
             Qurae is an emerging company from the University of Nottingham, 
             built on a foundation of delivering impactful programmes with the NHS, 
             Health Data Research UK, DARE UK, and the NIHR Nottingham Biomedical 
             Research Centre.
           </BodyText>
 
-          <BodyText>
+          <BodyText className="text-white">
           We enable organisations to harness federated technologies to drive collaboration, insight, and innovation. 
           By uniting technical expertise with governance and policy understanding, we help clients turn complex data landscapes into connected ecosystems.
           </BodyText>
 
-          <BodyText>
+          <BodyText className="text-white">
             Our mission is to help organisations become federation ready, enabling 
             them to unlock the power of collaborative data analysis while respecting 
             privacy, security, and regulatory requirements.
